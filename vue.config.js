1348cfe0dd3e7af31e384b133f14f03f4f38474f
AshTaylor1 <47848818+AshTaylor1@users.noreply.github.com>
2019-10-22 13:44:37 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@
 module.exports = {
   chainWebpack: config => {
     config.plugin('define').tap(args => {
-      args[0].graphendpoint = JSON.stringify('https://demo.viosystems.com/grcapi/')
+      args[0].graphendpoint = JSON.stringify('https://neo4j.viosystems.com/grcapi/')
       return args
     })
   }
