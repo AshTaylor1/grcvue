f52bd3025b60fee0c2ee8524ff8a86b03570c51b
Jim Lack <jlack35@gmail.com>
2019-10-22 13:24:51 +0100

    hardwire service location

--- src/components/GraphDisplay.vue
+++ src/components/GraphDisplay.vue
@@ -10,9 +10,9 @@
   import D3Network from 'vue-d3-network'
   import axios from 'axios'
 
-  var url = "https://demo.viosystems.com/grcapi/";
+  var url = "https://demo.viosystems.com/grcapi/graph";
   if (graphendpoint != undefined)
-    url = graphendpoint + 'graph';
+    url = graphendpoint + 'graph/';
 
   export default {
     name: "GraphDisplay",
