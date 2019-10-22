9600d199bc343d576e16009643f30b6d4c41deff
Jim Lack <jlack35@gmail.com>
2019-10-22 12:04:30 +0100

    change root context

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 
 
 module.exports = {
+  publicPath: 'grcvue',
   chainWebpack: config => {
     config.plugin('define').tap(args => {
       args[0].graphendpoint = JSON.stringify(process.env.graphendpoint)
